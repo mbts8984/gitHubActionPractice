9838d482bacfb2c5f394669cdb46ebdbea393c9f
mbts8984 <maddie.morton@mastery.net>
2021-08-18 11:27:29 -0500

    ad

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 module.exports = ({github, context}) => {
-    console.log("MADE IT TO SCRIPTS")
+    console.log("MADE IT TO SCRIPTS print")
     console.log('Context print ', context)
     return context.payload.client_payload.value
   }
